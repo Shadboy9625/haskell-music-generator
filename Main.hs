be7d216a361766184c71792a2db88ff854858ad9
Edson <shadabkalim375@gmail.com>
2023-11-15 16:56:03 +0530

    added comments

--- Main.hs
+++ Main.hs
@@ -1,3 +1,5 @@
+-- | Music Generation in Haskell: Happy Birthday Melody
+
 module Main where
 
 import qualified Data.ByteString.Lazy as B
@@ -6,7 +8,7 @@ import Data.Foldable
 import System.Process
 import Text.Printf
 
--- Type synonyms for sound properties
+-- | Type synonyms for sound properties
 type Pulse = Float
 type Seconds = Float
 type Samples = Float
@@ -14,39 +16,39 @@ type Hz = Float
 type Semitones = Float
 type Beats = Float
 
--- File path to save the generated music
+-- | File path to save the generated music
 outputFilePath :: FilePath
 outputFilePath = "output.bin"
 
--- Adjust the volume to prevent distortion
+-- | Adjust the volume to prevent distortion
 volume :: Float
 volume = 0.3
 
--- Sampling rate (number of samples per second)
+-- | Sampling rate (number of samples per second)
 sampleRate :: Samples
 sampleRate = 48000.0
 
--- Standard pitch (A4 = 440 Hz)
+-- | Standard pitch (A4 = 440 Hz)
 pitchStandard :: Hz
 pitchStandard = 440.0
 
--- Beats per minute (tempo)
+-- | Beats per minute (tempo)
 bpm :: Beats
 bpm = 120.0
 
--- Duration of one beat in seconds
+-- | Duration of one beat in seconds
 beatDuration :: Seconds
 beatDuration = 60.0 / bpm
 
--- Function to calculate frequency in Hz for a given number of semitones
+-- | Function to calculate frequency in Hz for a given number of semitones
 f :: Semitones -> Hz
 f n = pitchStandard * (2 ** (1.0 / 12.0)) ** n
 
--- Function to generate a note for a specific number of semitones and duration in beats
+-- | Function to generate a note for a specific number of semitones and duration in beats
 note :: Semitones -> Beats -> [Pulse]
 note n beats = freq (f n) (beats * beatDuration)
 
--- Function to create a sine wave of specific frequency and duration
+-- | Function to create a sine wave of specific frequency and duration
 freq :: Hz -> Seconds -> [Pulse]
 freq hz duration =
   map (* volume) $ zipWith3 (\x y z -> x * y * z) release attack output
@@ -54,15 +56,15 @@ freq hz duration =
     step = (hz * 2 * pi) / sampleRate
     -- Generate an attack to make notes smoother
     attack :: [Pulse]
-    attack = map (min 1.0) [0.0, 0.01 ..] -- Increased attack time
+    attack = map (min 1.0) [0.0, 0.01 .. 0.1] -- Increased attack time
     -- Generate a release to smooth the end of the sound
     release :: [Pulse]
-    release = reverse $ take (length output) attack
+    release = map (* last attack) $ reverse attack -- Mimicking attack for release
     -- Generate the actual sound wave (sine wave)
     output :: [Pulse]
     output = map sin $ map (* step) [0.0 .. sampleRate * duration]
 
--- Melody for 'Happy Birthday' song
+-- | Melody for 'Happy Birthday' song
 happyBirthday :: [Pulse]
 happyBirthday =
   concat
@@ -72,11 +74,11 @@ happyBirthday =
       note 10 0.5, note 10 0.5, note 9 1.0, note 5 1.0, note 7 1.0, note 5 2.0 -- Happy Birthday to you
     ]
 
--- Function to save the generated music to a binary file
+-- | Function to save the generated music to a binary file
 save :: FilePath -> [Pulse] -> IO ()
 save filePath pulses = B.writeFile filePath $ B.toLazyByteString $ fold $ map B.floatLE pulses
 
--- Function to play the generated music
+-- | Function to play the generated music
 play :: IO ()
 play = do
   save outputFilePath happyBirthday
@@ -84,6 +86,6 @@ play = do
   _ <- runCommand $ printf "ffplay -autoexit -showmode 1 -f f32le -ar %f %s" sampleRate outputFilePath
   return ()
 
--- Entry point of the program
+-- | Entry point of the program
 main :: IO ()
 main = play
